1c424bcd6080e074bec6f53fd1be4f0f660ac83d
Ana-Paula-Guz <>
2024-01-13 12:18:01 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,24 @@ for (let button of buttons) {
             alert("You ckicked Submit!")
         } else {
            let gameType= this.getAttribute("data-type")
-           alert(`You clicked ${gameType}`)
+           runGame(gameType);
         }
     })
 }
+
+   runGame("addition");
 })
 
-function runGame() {
+function runGame(gameType) {
     //Create two random num between 1 and 25 /
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert (`Unknown game Type: ${gameType}`)
+        throw `Unknown game Type: ${gameType}`;
+    }
 }
 
 function checkAnswer() {
@@ -34,8 +42,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent= operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";  
 }
 
 function displaySubtractQuestion() {
